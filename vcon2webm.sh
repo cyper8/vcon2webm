4aec8c1a70a828927138377b60b1432e891f7d0e
Anton Yashchuk <anton.yashchuk@gmail.com>
2018-05-21 12:19:34 +0300

    added mp4 conversion based on extension of the filename given as the second argument

--- vcon2webm.sh
+++ vcon2webm.sh
@@ -9,23 +9,43 @@ if [ "$CORES" -gt "1" ]; then
   CORES="$(($CORES - 1))"
 fi
 #echo "conv ${IN} to ${OUT} in $CORES threads (argc:$#)"
-#echo "--- webm, First Pass"
-ffmpeg -i "${IN}" \
+if echo "$OUT" | grep -Eiq ".mp4$"; then
+  #echo "--- x264, First Pass"
+  ffmpeg -i "${IN}" \
+    -hide_banner -loglevel error -stats -strict experimental \
+    -codec:v libx264 -threads 0 -profile:v baseline -level 3.0 -preset slow \
+    -b:v 1500k -maxrate 1500k -bufsize 3000k -pix_fmt yuv420p \
+    -codec:a aac -b:a 192k \
+    -pass 1 \
+    -f mp4 \
+    -y /dev/null
+  #echo "--- x264, Second Pass"
+  ffmpeg -i "${IN}" \
+    -hide_banner -loglevel error -stats -strict experimental -movflags +faststart \
+    -codec:v libx264 -threads 0 -profile:v baseline -level 3.0 -preset slow \
+    -b:v 1500k -maxrate 1500k -bufsize 3000k -pix_fmt yuv420p \
+    -codec:a aac -b:a 192k \
+    -pass 2 \
+    -y "${OUT}"
+else
+  #echo "--- webm, First Pass"
+  ffmpeg -i "${IN}" \
     -hide_banner -loglevel error -stats \
     -codec:v libvpx -threads $CORES -slices 4 -deadline good -cpu-used 4 \
-    -b:v 1500k -crf 10 -qmin 0 -qmax 42 -maxrate 1800k -bufsize 4000k -pix_fmt yuv420p \
-    -codev:a libvorbis -qscale:a 6 \
+    -b:v 1500k -crf 10 -qmin 10 -qmax 42 -maxrate 1500k -bufsize 3000k -pix_fmt yuv420p \
+    -codec:a libvorbis -qscale:a 6 \
     -pass 1 \
     -f webm \
     -y /dev/null
 
-#echo "--- webm, Second Pass"
-ffmpeg -i "${IN}" \
-    -hide_banner -loglevel error -stats \
+  #echo "--- webm, Second Pass"
+  ffmpeg -i "${IN}" \
+    -hide_banner -loglevel error -stats -movflags +faststart \
     -codec:v libvpx -threads $CORES -slices 4 -deadline good -cpu-used 1 \
-    -b:v 1500k -crf 10 -qmin 0 -qmax 42 -maxrate 1800k -bufsize 4000k -pix_fmt yuv420p \
+    -b:v 1500k -crf 10 -qmin 10 -qmax 42 -maxrate 1500k -bufsize 3000k -pix_fmt yuv420p \
     -auto-alt-ref 1 -lag-in-frames 25 \
     -codec:a libvorbis -qscale:a 6 \
     -pass 2 \
     -f webm \
     -y "${OUT}"
+fi
